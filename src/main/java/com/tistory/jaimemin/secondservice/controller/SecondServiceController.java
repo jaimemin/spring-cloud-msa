db5fa0534ca802ac6f9e95649453879e1b57242e
jaimemin <jaimemin@gmail.com>
2022-10-17 16:30:16 +0900

    tested load balancing

--- src/main/java/com/tistory/jaimemin/secondservice/controller/SecondServiceController.java
+++ src/main/java/com/tistory/jaimemin/secondservice/controller/SecondServiceController.java
@@ -1,15 +1,22 @@
 package com.tistory.jaimemin.secondservice.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/second-service/")
 public class SecondServiceController {
+
+    private final Environment environment;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -24,7 +31,10 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from Second Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from Second Service on PORT %s."
+                , environment.getProperty("local.server.port"));
     }
 }
