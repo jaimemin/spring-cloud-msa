e2f4bbf9054bb7d39625eac5ea00cc44376cf797
jaimemin <jaimemin@gmail.com>
2022-10-17 12:40:04 +0900

    initial commit

---
+++ src/test/java/com/tistory/jaimemin/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jaimemin.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
