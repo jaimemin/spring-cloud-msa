ed946699e0c6c3b0c5d3907eada380db9edc7457
jaimemin <jaimemin@gmail.com>
2022-10-17 16:29:51 +0900

    applied load balancing

--- src/main/java/com/tistory/jaimemin/apigatewayservice/ApigatewayServiceApplication.java
+++ src/main/java/com/tistory/jaimemin/apigatewayservice/ApigatewayServiceApplication.java
@@ -2,7 +2,9 @@ package com.tistory.jaimemin.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
